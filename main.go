b6f95d79ed1e31e52d7591434f34b18f8a26e4b6
Kyle Lomer <kylelomer@gmail.com>
2026-05-03 23:54:03 +0200

    Concurrent client requests

--- main.go
+++ main.go
@@ -4,27 +4,65 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/k-lomer/lights-out/clients"
+	"github.com/k-lomer/lights-out/model"
 )
 
 var client *http.Client = &http.Client{
 	Timeout: 30 * time.Second,
 }
 
-func ListHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("called list handler")
+func aggregateOutages(outages *[][]model.Outage) []model.Outage {
+	var totalOutages []model.Outage
+	for _, r := range *outages {
+		totalOutages = append(totalOutages, r...)
+	}
+	return totalOutages
+}
 
-	outages, err := clients.ListNorthernPowergridOutages(r.Context(), client)
-	if err != nil {
-		log.Printf("error getting NortherPowergrid outages: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func ListHandler(w http.ResponseWriter, r *http.Request) {
+	var clientResults [][]model.Outage
+	var wg sync.WaitGroup
+	clientProviders := 2
+	clientErrors := 0
+	wg.Add(clientProviders)
+
+	go func() {
+		defer wg.Done()
+		outages, err := clients.ListNorthernPowergridOutages(r.Context(), client)
+		if err != nil {
+			log.Printf("error getting NortherPowergrid outages: %v", err)
+			clientErrors += 1
+			return
+		}
+		clientResults = append(clientResults, outages)
+	}()
+
+	go func() {
+		defer wg.Done()
+		outages, err := clients.ListSseOutages(r.Context(), client)
+		if err != nil {
+			log.Printf("error getting SSE outages: %v", err)
+			clientErrors += 1
+			return
+		}
+		clientResults = append(clientResults, outages)
+	}()
+
+	wg.Wait()
+
+	if clientErrors == clientProviders {
+		http.Error(w, "all DNO clients failed", http.StatusInternalServerError)
 		return
 	}
 
+	totalOutages := aggregateOutages(&clientResults)
+
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(outages); err != nil {
+	if err := json.NewEncoder(w).Encode(totalOutages); err != nil {
 		log.Printf("error encoding outages: %v", err)
 	}
 }
