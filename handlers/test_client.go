b3b8ce44d20052ae69a5ec89926ac7d0c0836625
Kyle Lomer <kylelomer@gmail.com>
2026-07-18 23:39:28 +0100

    Use UTC time for test client

--- handlers/test_client.go
+++ handlers/test_client.go
@@ -30,9 +30,10 @@ func (t TestDnoClient) GetDno() model.Dno {
 
 func (t TestDnoClient) ListOutages(ctx context.Context) ([]model.Outage, error) {
 	outages := make([]model.Outage, 0, t.NumOutages)
-	lastUpdated := time.Now()
+	lastUpdated := time.Now().UTC()
 	for i := range t.NumOutages {
 		start, _ := time.Parse(time.RFC3339, "2026-07-05T15:00:00Z")
+		start = start.UTC()
 		end := start.Add(30 * time.Hour)
 		p, _ := model.NewPostcode(fmt.Sprintf("N%d %dAA", i%100, i%10))
 		postcodes := []model.Postcode{p}
